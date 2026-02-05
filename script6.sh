a282cd12b947a2879532d80bf45cfc627359fbb0
Adrian <dominguez.obrero.adrian@iescamas.es>
2026-02-05 13:34:52 +0100

    Terminado 05/02/2026

--- script6.sh
+++ script6.sh
@@ -3,15 +3,22 @@
 # En el que se crea el fichero pasado como primer parametro en el directorio pasado como segundo parametro. 
 # Se debe de comprobar en cada momento si el fichero ya existe o si el directorio ya existe
 
-crearDir=`mkdir $2`
-crearFich=`touch $1`
-acceder=`cd $2`
-
-if test -d $2
+if test -d "$2"
 then
    echo El directorio ya existe
 else
-   
+   mkdir "$2"
+   echo Directorio creado correctamente
+fi
+
+cd "$2"
+
+if test -e "$1"
+then 
+   echo El fichero ya existe
+else
+   touch "$1"
+   echo Fichero creado correctamente
 fi
 
 
--- ver.sh
+++ ver.sh
@@ -3,3 +3,14 @@
 # En el que si le pasas como parametro la palabra usuarios como parametro te dara un listado con 
 # los usuarios del sistema(solo su nombre) y si la palabra pasada es configuracion dara un listado de los ficheros
 # de la carpeta /etc
+
+
+if [ $1 == "usuarios" ]
+then
+   cat /etc/passwd | cut -d ":" -f 1
+elif [ $1 == "configuracion" ]
+then
+   ls /etc
+else
+   echo "Usa: $0 {usuarios|configuracion}"
+fi
