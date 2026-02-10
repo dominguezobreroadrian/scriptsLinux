39e9ef920df79824f4bb8ad05a20b86519db175c
Adrian <dominguez.obrero.adrian@iescamas.es>
2026-02-10 12:55:41 +0100

    corregido

--- script6.sh
+++ script6.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# En el que se crea el fichero pasado como primer parametro en el directorio pasado como segundo parametro. 
+# En el que se crea el fichero pasado como primer parametro en el directorio pasado como segundo parametro.
 # Se debe de comprobar en cada momento si el fichero ya existe o si el directorio ya existe
 
 if test -d "$2"
@@ -14,7 +14,7 @@ fi
 cd "$2"
 
 if test -e "$1"
-then 
+then
    echo El fichero ya existe
 else
    touch "$1"
