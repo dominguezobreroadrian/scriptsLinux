5249983f286e1ab6fa2b515df3b2f462518a6d81
Adrian <dominguez.obrero.adrian@iescamas.es>
2026-02-10 13:05:28 +0100

    corregido

--- script6.sh
+++ script6.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Autor: Adrián Domínguez Obrero
+
 # En el que se crea el fichero pasado como primer parametro en el directorio pasado como segundo parametro.
 # Se debe de comprobar en cada momento si el fichero ya existe o si el directorio ya existe
 
