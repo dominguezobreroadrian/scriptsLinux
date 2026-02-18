5c0d3aa44629843726a5ac1c8b4b0e41c28fd177
Adrián Domínguez <dominguez.obrero.adrian@iescamas.es>
2026-02-18 13:54:32 +0100

    Fix script to create and compress tar file correctly

--- scriptPrac2.2.sh
+++ scriptPrac2.2.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
-read -p "Introduce el fichero resultante": nombreCompri
+#!/bin/bash
 
-while [ -e $nombreCompri  ]
+read -p "Introduce el fichero resultante: " nombreCompri
+
+while [ -e "$nombreCompri.tar.gz" ]
 do
-	read -p "Introduce el fichero resultante": nombreCompri
+    read -p "El fichero ya existe. Introduce otro nombre: " nombreCompri
 done
 
-tar -czvf $nombreCompri.tar.gz --files-from /dev/null
+# Crear tar vacío
+tar -cf "$nombreCompri.tar" --files-from /dev/null
 
-read -p "Cuantos ficheros quiere insertar": numero
+read -p "Cuantos ficheros quiere insertar: " numero
 
-for i in $numero
+for ((i=1; i<=numero; i++))
 do
-	read -p "Introduce el fichero a meter": nombreFich
-	if [ -e $nombreFich  ]
-	then
-		gunzip $nombreCompri.tar.gz
-		tar -rvf $nombreCompri.tar $nombreFich
-		gzip $nombreCompri.tar
-	fi
+    read -p "Introduce el fichero a meter: " nombreFich
+    
+    if [ -e "$nombreFich" ]
+    then
+        tar -rf "$nombreCompri.tar" "$nombreFich"
+    else
+        echo "El fichero no existe"
+    fi
 done
+
+# Comprimir al final
+gzip "$nombreCompri.tar"
+
+echo "Archivo $nombreCompri.tar.gz creado correctamente"
