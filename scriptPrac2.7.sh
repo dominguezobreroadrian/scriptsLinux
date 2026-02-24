c252e449bf8adc8cef70424dbd5363fa1204d816
Adrian <dominguez.obrero.adrian@iescamas.es>
2026-02-24 18:39:33 +0100

    terminado

--- scriptPrac2.7.sh
+++ scriptPrac2.7.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
 # Que te permita saber cuantos usuarios existen en el sistema pero que no son del propio sistema sino
-# creados por usuarios con privilegios de root(estos usuarios osn aquellos cuyo UID es 1000 o superior)
+# creados por usuarios con privilegios de root(estos usuarios son aquellos cuyo UID es 1000 o superior)
 # El resultado sera el numero de usuario y sus nombres
+
+
+total=`awk -F: '$3 >= 1000 { print $1 }' /etc/passwd | wc -l`
+
+echo La lista de usuarios con UID mayor igual que 1000 es:
+
+awk -F: '$3 >= 1000 { print $1 }' /etc/passwd
+
+echo En /etc/passwd hay "$total" usuarios creados por usuarios con privilegios de root
