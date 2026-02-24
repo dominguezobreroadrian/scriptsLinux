55ddf2500092ebf323aaada36491816a54f60e0b
Adrian <dominguez.obrero.adrian@iescamas.es>
2026-02-24 14:38:39 +0100

    hacer en casa

---
+++ scriptPrac2.7.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Que te permita saber cuantos usuarios existen en el sistema pero que no son del propio sistema sino
+# creados por usuarios con privilegios de root(estos usuarios osn aquellos cuyo UID es 1000 o superior)
+# El resultado sera el numero de usuario y sus nombres
