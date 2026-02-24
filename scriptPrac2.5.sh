c7dde404b4bd058cadcc97329dd8bbe57927bb72
Adrián Domínguez <dominguez.obrero.adrian@iescamas.es>
2026-02-24 13:32:10 +0100

    Implement addition and multiplication operations
    Fix the script to correctly read and process operation type.

--- scriptPrac2.5.sh
+++ scriptPrac2.5.sh
@@ -2,6 +2,30 @@
 
 # Permita hacer la suma o el producto de todos los valores pasados por parámetros
 
-read -p "Introduce que quieres hacer con los valores insertados sumar o multiplicar": entrada
+operacion=$1
+shift   # Elimina el primer parámetro (suma o producto)
 
-if [  ]
+if [ "$operacion" = "suma" ]
+then
+    resultado=0
+fi
+
+if [ "$operacion" = "producto" ]
+then
+    resultado=1
+fi
+
+for numero in $@
+do
+    if [ "$operacion" = "suma" ]
+    then
+        resultado=`expr $resultado + $numero`
+    fi
+
+    if [ "$operacion" = "producto" ]
+    then
+        resultado=`expr $resultado \* $numero`
+    fi
+done
+
+echo "El resultado es: $resultado"
