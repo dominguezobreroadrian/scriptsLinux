eff10f69d4e5bbf6c60bfddb8b9a3c4c89d4c9eb
Adrian <dominguez.obrero.adrian@iescamas.es>
2026-02-24 13:26:02 +0100

    repaso

---
+++ scriptPrac2.5.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Permita hacer la suma o el producto de todos los valores pasados por parámetros
+
+read -p "Introduce que quieres hacer con los valores insertados sumar o multiplicar": entrada
+
+if [  ] 
